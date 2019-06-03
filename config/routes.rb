8b341fc196dea8f8a0d9465c18d4fe915df4eadd
Dylan Michael Labat <dml252@live.com>
2019-06-02 21:10:49 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :songs, only: [:index, :show, :new, :create, :update, :edit]
+  resources :genres, only: [:index, :show, :new, :create, :update, :edit]
+  resources :artists, only: [:index, :show, :new, :create, :update, :edit]
 end
