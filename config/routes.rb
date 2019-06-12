e24afefb8ea499d915b6e15479dfae1ef9e38fbc
imaglazer <avigayilglazer@gmail.com>
2019-06-12 16:55:14 +0300

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,49 @@
 Rails.application.routes.draw do
+  resources :songs
+  resources :genres
+  resources :artists
+  
+  # get 'songs/new'
+
+  # get 'songs/create'
+
+  # get 'songs/edit'
+
+  # get 'songs/update'
+
+  # get 'songs/destroy'
+
+  # get 'songs/index'
+
+  # get 'songs/show'
+
+  # get 'genres/new'
+
+  # get 'genres/create'
+
+  # get 'genres/edit'
+
+  # get 'genres/update'
+
+  # get 'genres/destroy'
+
+  # get 'genres/index'
+
+  # get 'genres/show'
+
+  # get 'artists/new'
+
+  # get 'artists/create'
+
+  # get 'artists/edit'
+
+  # get 'artists/update'
+
+  # get 'artists/destroy'
+
+  # get 'artists/index'
+
+  # get 'artists/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
